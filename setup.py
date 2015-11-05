f647411bc6b8ff013f0d3d21ae550c69409e93cc
h4llow3En <Doering.Felix@googlemail.com>
2015-11-05 16:41:51 +0100

    Add uninstall and cleanup
    The cleanup is called after install and after uninstall

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 from setuptools import setup, find_packages
 import sys
 import os
+import subprocess
 
 
 version = sys.version_info[:2]
@@ -13,13 +14,47 @@ if version < (3, 4):
 VERSION = 'none'
 
 
-setup(name='tdo',
-      version=VERSION,
-      description="A todo list tool for the terminal.",
-      author='Felix Wittwer',
-      license='MIT',
-      packages=find_packages(),
-      include_package_data=True,
-      zip_safe=False,
-      entry_points={'console_scripts': [
-          'tdo = tdo.main:main']})
+def install():
+    setup(name='tdo',
+          version=VERSION,
+          description="A todo list tool for the terminal.",
+          author='Felix Wittwer',
+          license='MIT',
+          packages=find_packages(),
+          include_package_data=True,
+          zip_safe=False,
+          entry_points={'console_scripts': [
+              'tdo = tdo.main:main']})
+    filelist = ['{dir}/build'.format(dir=os.getcwd()),
+                '{dir}/dist'.format(dir=os.getcwd()),
+                '{dir}/tdo.egg-info'.format(dir=os.getcwd())]
+
+    cleanup(filelist)
+
+
+def uninstall():
+    subprocess.call([sys.argv[0], 'install', '--record', 'files.txt'])
+    filelist = []
+    with open('files.txt') as files:
+        filelist = files.readlines()
+    for element in range(len(filelist)):
+        filelist[element] = filelist[element][:-1]
+    filelist.append('{dir}/.tdo'.format(dir=os.environ['HOME']))
+    filelist.append('{dir}/build'.format(dir=os.getcwd()))
+    filelist.append('{dir}/dist'.format(dir=os.getcwd()))
+    filelist.append('{dir}/tdo.egg-info'.format(dir=os.getcwd()))
+    filelist.append('{dir}/files.txt'.format(dir=os.getcwd()))
+
+    cleanup(filelist)
+
+
+def cleanup(filelist):
+    for entry in filelist:
+        subprocess.call(['rm', '-rf', entry])
+
+
+if __name__ == '__main__':
+    if sys.argv[1] == 'uninstall':
+        uninstall()
+    else:
+        install()
