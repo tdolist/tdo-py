4b5aa16e8d26b29ae5b8e0db05e6d7b04705c8f9
h4llow3En <Doering.Felix@googlemail.com>
2015-11-12 11:46:58 +0100

    export just the lists you want

--- tdo/todolist/export.py
+++ tdo/todolist/export.py
@@ -1,3 +1,4 @@
+import sys
 from .listing import sortdict
 
 
@@ -7,22 +8,47 @@ def mdexport(todolist, exportfile):
     if not os.path.isdir(path):
         print('The given output path does not exist!')
     else:
-        print('Exporting your todos to "{filename}"...'.format(
-            filename=exportfile))
         exportstring = '# Your todos:\n\n'
-        keylist = sortdict(todolist)
-        for key in keylist:
-            exportstring += '## {listname}\n\n'.format(listname=key)
-            idlist = sortdict(todolist[key], id=True)
-            for todo_id in idlist:
-                if todolist[key][todo_id][1]:
-                    done = 'x'
-                else:
-                    done = ' '
-                exportstring += '  - [{done}] {todoname}\n'.format(
-                    done=done,
-                    todoname=todolist[key][todo_id][0])
-            exportstring += '\n'
-        with open(exportfile, 'w') as f:
-            f.write(exportstring)
-        print('Done.')
+        if len(sys.argv) < 4:
+            print('Exporting your todos to "{filename}"...'.format(
+                filename=exportfile))
+            keylist = sortdict(todolist)
+            for key in keylist:
+                exportstring += '## {listname}\n\n'.format(listname=key)
+                idlist = sortdict(todolist[key], id=True)
+                for todo_id in idlist:
+                    exportstring += addtoexport(todolist[key][todo_id])
+                exportstring += '\n'
+            with open(exportfile, 'w') as f:
+                f.write(exportstring)
+            print('Done.')
+        else:
+            keylist = sys.argv[3:]
+            notinlist = False
+            for key in keylist:
+                if key not in todolist:
+                    print('You have no list named {nolist}'.format(nolist=key))
+                    notinlist = True
+            if notinlist:
+                return
+            else:
+                for key in keylist:
+                    exportstring += '## {listname}\n\n'.format(listname=key)
+                    idlist = sortdict(todolist[key], id=True)
+                    for todo_id in idlist:
+                        exportstring += addtoexport(todolist[key][todo_id])
+                    exportstring += '\n'
+                print(exportstring)
+                print('Done.')
+
+
+
+
+def addtoexport(todolist):
+        if todolist[1]:
+            done = 'x'
+        else:
+            done = ' '
+        return'  - [{done}] {todoname}\n'.format(
+            done=done,
+            todoname=todolist[0])
